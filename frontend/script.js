116e4e81216604792280f6e203cecfeeab27fc12
Rajeswarirangaraj <rajeswarirlatha@gmail.com>
2025-12-24 15:28:00 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -7,18 +7,15 @@ function sendMessage() {
 
     let chatbox = document.getElementById("chatbox");
 
-    // Show user message
+    
     chatbox.innerHTML += `<p><b>You:</b> ${input}</p>`;
 
-    // Dummy bot reply (frontend only)
     let reply = "This message will come from backend later.";
 
     chatbox.innerHTML += `<p><b>Bot:</b> ${reply}</p>`;
 
-    // Clear input
     document.getElementById("userInput").value = "";
 
-    // Scroll to bottom
     chatbox.scrollTop = chatbox.scrollHeight;
 }
 
