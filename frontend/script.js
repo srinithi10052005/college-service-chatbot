f8e3d4363d01d4d443c8b18ad702c5118336f8d3
Srinithi <srinithibaskaran456@gmail.com>
2025-12-30 09:30:36 +0530

    Add showMessage function for welcome alert
    Added a welcome message alert function.

--- frontend/script.js
+++ frontend/script.js
@@ -1,21 +1,4 @@
-document.getElementById("sendBtn").addEventListener("click", sendMessage);
-
-function sendMessage() {
-    let input = document.getElementById("userInput").value;
-
-    if (input === "") return;
-
-    let chatbox = document.getElementById("chatbox");
-
-    
-    chatbox.innerHTML += `<p><b>You:</b> ${input}</p>`;
-
-    let reply = "This message will come from backend later.";
-
-    chatbox.innerHTML += `<p><b>Bot:</b> ${reply}</p>`;
-
-    document.getElementById("userInput").value = "";
-
-    chatbox.scrollTop = chatbox.scrollHeight;
+function showMessage() {
+    alert("Welcome to Service Assistant!");
 }
 
